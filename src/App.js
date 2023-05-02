ef160c4aa43a8c6098f043f0dab05c9d4efb97c8
Tamilselvan K <tamilselvanks007@gamil.com>
2023-05-02 12:07:43 +0530

    updated🎉

--- src/App.js
+++ src/App.js
@@ -7,23 +7,56 @@ import { TenantList } from "./TenantList";
 import { INITIAL_TENANT_LIST } from "./INITIAL_TENANT_LIST";
 import { AddTenant } from "./AddTenant";
 import { EditTenant } from "./EditTenant";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useHistory } from "react-router-dom";
+import HouseIcon from "@mui/icons-material/House";
 
 function App() {
   const [tenantList, setTenantList] = useState(INITIAL_TENANT_LIST);
-
+  const history = useHistory();
   return (
     <div className="App">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/tenants">Tenants</Link>
-        </li>
-        <li>
-          <Link to="/tenants/add">Add Tenants</Link>
-        </li>
-      </ul>
+      <AppBar position="static" color="error">
+        <Toolbar>
+          <HouseIcon
+            sx={{ display: { xs: "none", md: "flex", color: "black" }, mr: 1 }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 60,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            LET'S RENT
+          </Typography>
+          <div>
+            <Button color="inherit" onClick={() => history.push("/")}>
+              Home
+            </Button>
+            <Button color="inherit" onClick={() => history.push("/tenants")}>
+              Tenants
+            </Button>
+            <Button
+              color="inherit"
+              onClick={() => history.push("/tenants/add")}
+            >
+              Add Tenants
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
 
       <Switch>
         <Route exact path="/">
--- src/WelcomeMsg.js
+++ src/WelcomeMsg.js
@@ -1,7 +1,11 @@
+import Typography from '@mui/material/Typography';
+
 export function WelcomeMsg() {
   return (
-    <div>
-      <h2>Welcome to the House Management System!!</h2>
+    <div className="msg-container">
+      <div className="welcome-msg">
+        <Typography variant="h2">HOUSE FOR RENT</Typography>
+      </div>
     </div>
   );
 }
